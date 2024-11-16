6e4ae77a1db5cecaec0768b5b45a126f53f00fec
Umair <ch.umair111353@gmail.com>
2024-11-16 13:13:12 +0500

    Updated home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,55 @@
 import React from 'react'
-import { Link } from 'react-router-dom' 
+//import { Link } from 'react-router-dom' 
+import NavBar from '../Components/NavBar'
+import CityCard from '../Components/CityCard'
+import SearchBar from '../Components/SearchBar'
+import backGround from '../assets/backGround.webp'
+
 
 
 export default function home() {
   return (
-    <div>
-   
-        <p className='text-blue'>wellcome to home </p>
-          <Link to="/signup">go to signup</Link>
-          <br></br>
-          <Link to ="signin">go to signin</Link>
-       
+    <div className='bg-gray-900'>
+    <NavBar />
+    {/** Discover your spaces */}
+    <div className="relative w-full h-64">
+      {/* Background Image */}
+      <img
+        src={backGround}
+        alt="City view"
+        className="w-full h-full object-cover"
+      />
+
+      {/* Overlay Content */}
+      <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col items-center justify-center">
+        <h1 className="text-white text-2xl md:text-4xl font-bold mb-4">
+          Discover Your Space
+        </h1>
+        <SearchBar />
+      </div>
+    </div>
+    {/**Rent Around pakistan */}
+    <div className="py-8 px-4">
+        <h2 className="text-center text-white text-xl md:text-3xl font-bold mb-4">Rent Around Pakistan</h2>
+        <p className="text-center text-gray-400 mb-8">
+          Find apartments, rooms, and buildings for rent all across Pakistan.
+        </p>
+    </div>
+     
+     
+
+        <CityCard />
+
+    {/**  list your property */}
+    <div className="text-center bg-gray-900 text-white py-8 px-4">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4">List Your Rental on Nivaas.</h2>
+          <p className="text-gray-400 mb-6">
+             List your property on Nivaas to connect with trusted renters easily. Get started today!
+          </p>
+            <button className="bg-white text-black px-6 py-3 rounded-full font-bold">
+             List a Property for Free
+            </button>
+    </div>
     </div>
   )
 }
