eea84514d86e72de15517cc2470aada488f712b0
Umair <ch.umair111353@gmail.com>
2024-11-23 11:18:57 +0500

    updated button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,14 +1,13 @@
 // Button.js
 const Button = ({ onClick, text }) => {
-    return (
-      <button
-        onClick={onClick}
-        className="w-full p-3 bg-white text-dark-gray font-semibold rounded-md mt-4"
-      >
-        {text}
-      </button>
-    );
-  };
-  
-  export default Button;
-  
+  return (
+    <button
+      onClick={onClick}
+      className="w-full p-3 bg-white text-dark-grey font-semibold rounded-md mt-4"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
